7775b1a06af651331bc1eb1deef1f590760651de
alaa202001 <alaazidan6200@gmail.com>
2023-09-22 06:18:50 +0300

    leson3

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,17 @@
 import './App.css'
 
 function App() {
+  let person = "alaa zidan";
+  const changeName = () => {
+    person = "cabitano";
+  };
 
 
   return (
     <>
       <div>
-      
-    
-      
-        <button>
-          count is 0
-        </button>
+        <h2>M Name Is {person}</h2>
+        <button onClick={changeName}>count is 0</button>
         
       </div>
     
