be8d505072dbea7289e81759977dbe366d90d274
alaa202001 <alaazidan6200@gmail.com>
2023-09-22 07:10:43 +0300

    leson2

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,37 @@
 
 import './App.css'
+import { useState } from "react";
 
 function App() {
-  let person = "alaa zidan";
-  const changeName = () => {
-    person = "cabitano";
-  };
+  // useState
+  const [name , setName] = useState("Alaa Zidan");
+  const [age , setAge] = useState (22)
+  const [count, setCount] = useState(0);
+
+
+  // countfunction
+  const changeCount = () => {
+    setCount(count+1);
+  }
+
+
 
 
   return (
     <>
       <div>
-        <h2>M Name Is {person}</h2>
-        <button onClick={changeName}>count is 0</button>
+        <h2>M Name Is {name}</h2>
+        <button onClick={() =>{setName("Capitano")}}>Change Name</button>
         
       </div>
+      <br />
+      <br />
+
+      <h2>My Age Ise {age}</h2>
+      <button onClick={() => {setAge(23)}}>Chanege Age</button>
+      <br />
+      <br />
+      <button onClick={changeCount}>Count is {count}</button>
     
     </>
   )
